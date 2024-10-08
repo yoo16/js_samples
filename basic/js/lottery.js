a6cfe99193b496f8bd158b12f5007fe622997407
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-09 05:17:20 +0900

    update

--- basic/js/lottery.js
+++ basic/js/lottery.js
@@ -7,44 +7,83 @@ const rarities = [
 
 var totalCount = 0;
 var counts = { N: 0, R: 0, SR: 0, SSR: 0 };
+const interval = 1000;
 
-const getRandomItemByProbability = (items) => {
-    var totalProbability = items.reduce((sum, item) => sum + item.probability, 0);
+// Element
+var cardImage = document.getElementById('card-image');
+var loadingModal = document.getElementById('loading-modal');
+
+/**
+ * ランダムレアリティ
+ */
+const getRandomRarity = () => {
+    // 確率合計値
+    var totalProbability = rarities.reduce((sum, item) => sum + item.probability, 0);
+
+    // ランダムな数字
     var randomValue = Math.random() * totalProbability;
+
+    // 確率累積値
     var accumulatedProbability = 0;
 
-    for (var item of items) {
-        accumulatedProbability += item.probability;
+    // カード種類で繰り返し
+    for (var rarity of rarities) {
+        accumulatedProbability += rarity.probability;
+        // ランダムな数字が、確率累積値より小さければレアリティ決定
         if (randomValue <= accumulatedProbability) {
-            return item;
+            return rarity;
         }
     }
 };
 
+/**
+ * カードを引く
+ */
 const play = () => {
-    var cardImage = document.getElementById('card-image');
-    var loadingModal = document.getElementById('loading-modal');
-
-    // モーダルを表示してローディングを開始
+    // モーダルローディング開始
     loadingModal.classList.remove('hidden');
+
+    // カード非表示
     cardImage.classList.remove('revealed');
 
-    // カードの切り替え処理
-    var item = getRandomItemByProbability(rarities);
-    var newCardImage = `./images/card-${item.type}.png`;
+    // レアリティ決定
+    var card = getRandomRarity();
 
+    // カード表示
+    showCard(card);
+};
+
+/**
+ * カードの切り替え処理
+ */
+const showCard = (card) => {
     setTimeout(() => {
         // 新しい画像をセット
+        var newCardImage = `./images/card-${card.type}.png`;
         cardImage.src = newCardImage;
         cardImage.classList.add('revealed');
 
-        // モーダルを非表示にする
+        // モーダル非表示
         loadingModal.classList.add('hidden');
 
-        // カウントの更新
+        // カウント更新
         totalCount++;
-        counts[item.type]++;
-        document.getElementById('total-count').innerHTML = totalCount;
-        document.getElementById(`count-${item.type}`).innerHTML = counts[item.type];
-    }, 2000);
-};
+        counts[card.type]++;
+        document.getElementById(`count-${card.type}`).innerHTML = counts[card.type];
+
+        // 集計
+        showTotaling();
+    }, interval);
+}
+
+/**
+ * 集計
+ */
+const showTotaling = () => {
+    for (const key in counts) {
+        // console.log(key, counts[key])
+        var per = (counts[key] / totalCount) * 100;
+        per = per.toFixed();
+        document.getElementById(`per-${key}`).innerHTML = per;
+    }
+}
