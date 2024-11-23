b694bb9bdaf61a9e45ecf261e8cb34c2640dc577
yoo16 <yohei.yoshikawa@gmail.com>
2024-11-23 16:06:23 +0900

    update

--- basic/js/dom4.js
+++ basic/js/dom4.js
@@ -1,44 +1,84 @@
-function addMessage() {
-    const newMessage = document.createElement("h4");
-    newMessage.innerText = "New Message";
-    document.getElementById("message_top").after(newMessage);
+function addNumberAppend() {
+    const message = document.createElement("p");
+    message.innerText = Math.floor(Math.random() * 100);
+    message.className = "text-blue-600 font-bold mt-2";
+
+    const info = document.createElement("span");
+    info.textContent = "More text"
+
+    // TODO: id=message-list の後に追加
+    document.getElementById("message-list").append(message, info);
 }
 
-function addCity() {
-    const cityArea = document.getElementById("city_area");
-    const newCity = document.createElement("div");
-    newCity.innerText = "City";
-    newCity.className = "p-2 bg-gray-200 rounded-md mb-2";
-    cityArea.appendChild(newCity);
+
+function addNumberAfter() {
+    const newNumber = document.createElement("div");
+    newNumber.innerText = Math.floor(Math.random() * 100);
+    newNumber.className = "text-blue-600 font-bold mt-2";
+
+    const info = document.createElement("span");
+    info.textContent = "More text"
+    // TODO: id=start の後に追加
+    document.getElementById("start").after(newNumber, info);
 }
 
-function removeCity() {
-    const cityArea = document.getElementById("city_area");
-    if (cityArea.lastChild) {
-        cityArea.removeChild(cityArea.lastChild);
+function addNumber() {
+    // id=item-name-1 のDOMから（親要素）、入力値を取得
+    const cityArea = document.getElementById("number-list");
+    const newItem = document.createElement("div");
+    newItem.innerText = Math.floor(Math.random() * 100);;
+    newItem.className = "p-2 bg-gray-200 rounded-md mb-2";
+
+    cityArea.appendChild(newItem);
+}
+
+
+function removeNumber() {
+    const numberList = document.getElementById("number-list");
+    // 最後の要素があれば
+    if (numberList.lastChild) {
+        // 最後の要素を削除
+        //const removedElement = numberList.removeChild(numberList.lastChild);
+        numberList.lastChild.remove();
     }
 }
 
 function addItem() {
-    const itemName = document.getElementById("item_name").value;
-    if (itemName) {
-        const newItem = document.createElement("li");
-        newItem.innerText = itemName;
-        document.getElementById("item_list").appendChild(newItem);
-        document.getElementById("item_name").value = ""; // 入力をクリア
+    // id=item-name のDOMから（親要素）、入力値を取得
+    const itemName = document.getElementById("item-name").value;
+    if (!itemName) {
+        alert("項目を入力してください。")
+        return
     }
+    // liタグ作成
+    const newItem = document.createElement("div");
+    // テキスト挿入
+    newItem.innerText = itemName;
+    newItem.className = "p-2 bg-gray-200 rounded-md mb-2";
+
+    newItem.onclick = function () {
+        this.classList.toggle("selected");
+        this.classList.toggle("bg-orange-200");
+    };
+
+    // クラス設定
+    newItem.classList.add('item');
+    // 親要素に liタグの追加
+    document.getElementById("item-list").appendChild(newItem);
+    // 入力をクリア
+    document.getElementById("item-name").value = "";
 }
 
 function removeItems() {
-    const itemList = document.getElementById("item_list");
-    while (itemList.firstChild) {
-        itemList.removeChild(itemList.firstChild);
-    }
-}
+    // class=selected の要素をすべて取得
+    const selectedItems = document.querySelectorAll(".selected");
 
-function addNumber() {
-    const newNumber = document.createElement("div");
-    newNumber.innerText = Math.floor(Math.random() * 100);
-    newNumber.className = "text-blue-600 font-bold mt-2";
-    document.getElementById("start").after(newNumber);
+    // 要素がなければ終了
+    if (!selectedItems) return;
+
+    // 子要素があれば、すべて削除
+    selectedItems.forEach(selectedItem => {
+        // document.getElementById("item-list").removeChild(selectedItem);
+        selectedItem.remove();
+    });
 }
---
+++ video/js/app.js
@@ -0,0 +1,183 @@
+const video = document.getElementById('video');
+const playPauseBtn = document.getElementById('playPauseBtn');
+const playIcon = document.getElementById('playIcon');
+const pauseIcon = document.getElementById('pauseIcon');
+const volumeOnIcon = document.getElementById('volumeOnIcon');
+const volumeMuteIcon = document.getElementById('volumeMuteIcon');
+const volumeSlider = document.getElementById('volumeSlider');
+const playbackSpeed = document.getElementById('playbackSpeed');
+const currentTimeDisplay = document.getElementById('currentTime');
+const durationDisplay = document.getElementById('duration');
+
+// ビデオスキップ（秒）
+const step = 5;
+// ビデオボリューム(0 - 1)
+const volume = 0.7;
+// ビデオファイル
+const videoFile = "videos/video1.mp4";
+
+/**
+ * loadVideo()
+ * ビデオ読み込み
+ */
+function loadVideo(filePath) {
+    video.pause();
+    video.src = filePath;
+    video.load();
+}
+
+/**
+ * onLoadedVideo()
+ * ビデオ読み込み後の処理
+ */
+function onLoadedVideo() {
+    console.log("can play")
+    video.volume = volume
+    volumeSlider.value = volume
+
+    updatePlayIcon();
+    updateDuration();
+    updateCurrentTime();
+}
+
+/**
+ * playPause()
+ * 再生＆停止
+ */
+function playPause() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+    updatePlayIcon();
+}
+
+/**
+ * updatePlayIcon()
+ * 再生アイコン更新
+ */
+function updatePlayIcon() {
+    if (video.paused) {
+        pauseIcon.classList.add("hidden"); // 停止アイコンを非表示
+        playIcon.classList.remove("hidden"); // 再生アイコンを表示
+    } else {
+        playIcon.classList.add("hidden"); // 再生アイコンを非表示
+        pauseIcon.classList.remove("hidden"); // 停止アイコンを表示
+    }
+}
+
+/**
+ * fullscreen()
+ * フルスクリーン
+ */
+function fullscreen() {
+    if (video.requestFullscreen) {
+        video.requestFullscreen();
+    } else if (video.webkitRequestFullscreen) { // Safari
+        video.webkitRequestFullscreen();
+    } else if (video.msRequestFullscreen) { // IE/Edge
+        video.msRequestFullscreen();
+    }
+}
+
+/**
+ * toggleMute()
+ * 音声ミュート切り替え
+ */
+function toggleMute() {
+    if (video.muted) {
+        video.muted = false;
+        volumeSlider.value = video.volume;
+    } else {
+        video.muted = true;
+        volumeSlider.value = 0;
+    }
+    // 音声アイコン更新
+    updateVolumeIcon();
+}
+
+/**
+ * changeVolume()
+ * 音声ボリューム変更
+ */
+function changeVolume(event) {
+    const volume = event.target.value;
+    // ボリューム設定
+    video.volume = volume;
+    // Mute判別
+    video.muted = volume == 0;
+    // 音声アイコン更新
+    updateVolumeIcon();
+}
+
+/**
+ * updateVolumeIcon()
+ * 音声アイコン更新
+ */
+function updateVolumeIcon() {
+    if (video.muted) {
+        // Muteの場合
+        volumeOnIcon.classList.add("hidden");
+        volumeMuteIcon.classList.remove("hidden");
+    } else {
+        // 音量がある場合
+        volumeMuteIcon.classList.add("hidden");
+        volumeOnIcon.classList.remove("hidden");
+    }
+}
+
+/**
+ * updateCurrentTime()
+ * 現在の時間更新
+ */
+function updateCurrentTime() {
+    const currentTime = Math.floor(video.currentTime);
+    const minutes = Math.floor(currentTime / 60);
+    const seconds = currentTime % 60;
+    currentTimeDisplay.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
+
+/**
+ * updateDuration()
+ * ビデオの長さ更新
+ */
+function updateDuration() {
+    const duration = Math.floor(video.duration);
+    const minutes = Math.floor(duration / 60);
+    const seconds = duration % 60;
+    durationDisplay.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
+
+/**
+ * skip()
+ * ビデオの時間スキップ
+ */
+function skip(seconds) {
+    video.currentTime += seconds;
+}
+
+/**
+ * changePlaybackSpeed()
+ * ビデオの再生速度変更
+ */
+function changePlaybackSpeed() {
+    video.playbackRate = parseFloat(playbackSpeed.value);
+}
+
+// キーボードイベント
+window.onkeydown = (event) => {
+    if (event.key === " ") {
+        event.preventDefault();
+        playPause();
+    } else if (event.key === "ArrowRight") {
+        event.preventDefault();
+        skip(step);
+    } else if (event.key === "ArrowLeft") {
+        event.preventDefault();
+        skip(-step);
+    }
+};
+
+// ビデオ読み込み
+loadVideo(videoFile);
