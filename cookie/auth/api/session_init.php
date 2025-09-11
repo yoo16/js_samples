85f3b03f2f44d83c830089ee7c5f51b734a1c94d
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-09-11 11:06:08 +0900

    update

--- cookie/auth/api/session_init.php
+++ cookie/auth/api/session_init.php
@@ -1,10 +1,10 @@
 <?php
 session_name('sid');
 session_set_cookie_params([
-    'lifetime' => 3600,
-    'path'     => '/',
+    'lifetime' => 3600,  // 1時間
+    'path'     => '/',   // 全パスで有効
     'secure'   => false, // 本番は true
-    'httponly' => true,
-    'samesite' => 'Lax',
+    'httponly' => true,  // HTTP Only: true の場合 JSから参照不可
+    'samesite' => 'Lax', // CSRF対策: Lax or Strict
 ]);
 session_start();
